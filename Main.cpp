723e632873be242838ffdd813a6897fad8f5b63d
ih1115 <inusha@outlook.com>
2017-07-23 21:50:10 +0100

    impletmented swap and basic code for calcualteMove

--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 #include <vector>
 
+#define BOARD_WIDTH 6
+#define BOARD_HEIGHT 12
+
+#define swap(board, x, y) {\
+                            board[x*BOARD_WIDTH + y] ^= board[x*BOARD_WIDTH + y + 1]; \
+                            board[x*BOARD_WIDTH + y + 1] ^= board[x*BOARD_WIDTH + y]; \
+                            board[x*BOARD_WIDTH + y] ^= board[x*BOARD_WIDTH + y + 1]; \
+                          }
+
 using namespace std;
 
 typedef vector<char> board_t;
@@ -24,10 +33,34 @@ board_t parseBoard()
   return board;
 }
 
+int clear(board_t board)
+{
+  // todo
+  return 0;
+}
+
 move_t calculateMove(board_t board)
 {
-  move_t move;
-  return move;
+  move_t bestMove;
+  int bestScore = -1;
+
+  for (int i = 0 ; i < BOARD_HEIGHT; i++)
+  {
+    for (int j = 0; j < BOARD_WIDTH - 1; j++) // only BOARD_WIDTH - 1 swaps possible per BOARD_WIDTH
+    {
+      board_t currentBoard(board);
+      swap(currentBoard, i, j);
+      int currentScore = clear(currentBoard);
+      if (currentScore > bestScore)
+      {
+        bestScore = currentScore;
+        bestMove.x = j;
+        bestMove.y = i;
+      }
+    }
+  }
+
+  return bestMove;
 }
 
 int main()
